bc47d258a3bdd5ec9714a20d7024613d02f1a54a
sharangabo <e.sharangab@alustudent.com>
2023-02-22 19:15:35 +0200

    commit

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-import csv
+"""Module"""
+
 import requests
 import sys
 
+if __name__ == '__main__':
+    employee_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+        .format(employee_id)
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+        .format(employee_id)
 
-if __name__ == "__main__":
-    if len(sys.argv) == 2 and sys.argv[1].isdigit():
-        user_id = int(sys.argv[1])
-        url = "https://jsonplaceholder.typicode.com"
-        response = requests.get(f"{url}/users/{user_id}")
-        if response.ok:
-            user = response.json()
-            response = requests.get(f"{url}/todos?userId={user_id}")
-            if response.ok:
-                todos = response.json()
-                with open(f"{user_id}.csv", mode="w") as csv_file:
-                    fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-                    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-                    writer.writeheader()
-                    for todo in todos:
-                        writer.writerow({
-                            "USER_ID": user["id"],
-                            "USERNAME": user["username"],
-                            "TASK_COMPLETED_STATUS": str(todo["completed"]),
-                            "TASK_TITLE": todo["title"]
-                        })
-                    print(f"{len(todos)} tasks have been exported to {user_id}.csv")
-            else:
-                print(f"Error: could not retrieve todos for user {user_id}")
-        else:
-            print(f"Error: could not retrieve user with id {user_id}")
-    else:
-        print("Usage: python3 1-export_to_CSV.py <user_id>")
+    user_info = requests.request('GET', user_url).json()
+    todos_info = requests.request('GET', todos_url).json()
+
+    employee_name = user_info["name"]
+    employee_username = user_info["username"]
+    task_completed = list(filter(lambda obj:
+                                 (obj["completed"] is True), todos_info))
+    number_of_done_tasks = len(task_completed)
+    total_number_of_tasks = len(todos_info)
+
+    with open(str(employee_id) + '.csv', "w") as file:
+        [file.write('"' + str(employee_id) + '",' +
+                    '"' + employee_username + '",' +
+                    '"' + str(task["completed"]) + '",' +
+                    '"' + task["title"] + '",' + "\n")
+         for task in todos_info]
